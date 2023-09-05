ba99b84738b30dc68af0c3706dddf9dcaa6af099
krdesai999 <krdesai999@gmail.com>
2023-09-05 14:22:39 -0700

    github config file added

---
+++ infra/config.js
@@ -0,0 +1,9 @@
+const githubConfig = {
+  owner: "krdesai999",
+  remoteURL: "https://github.com/krdesai999/fovus-project.git",
+  oauthToken: "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",
+  repository: "fovus-project",
+  productionBranch: "main",
+};
+
+exports.githubConfig = githubConfig;
