8120ef845f8233d61be1c627bf7b6a7e9045de58
krdesai999 <krdesai999@gmail.com>
2023-09-05 12:42:33 -0700

    routing for pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Authentication, { Auth } from "./components/Auth";
+import HomeLayout from "./layouts/HomeLayout";
+import Redirect from "./components/utils/Redirect";
+import { pages } from "./components/utils/PageDirection";
+import Form from "./pages/Form";
+import Login from "./pages/auth/Login";
+import SignUp from "./pages/auth/SignUp";
+import AuthLayout from "./layouts/AuthLayout";
+import ConfirmUser from "./pages/auth/ConfirmUser";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      element={
+        <Auth>
+          <HomeLayout />
+        </Auth>
+      }
+    >
+      {/* Redirect page for logged in or not users */}
+      <Route
+        index
+        element={<Redirect login={pages.auth.login} to={pages.form} />}
+      />
+
+      {/* Main input form */}
+      <Route path={pages.form} element={<Form />} />
+
+      {/* Authentication pages */}
+      <Route
+        element={
+          <Authentication>
+            <AuthLayout />
+          </Authentication>
+        }
+      >
+        <Route path={pages.auth.login} element={<Login />} />
+        <Route path={pages.auth.sign_up} element={<SignUp />} />
+        <Route path={pages.auth.confirm_user} element={<ConfirmUser />} />
+      </Route>
+    </Route>
+  )
+);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
