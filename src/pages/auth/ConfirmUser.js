b7f9eafe486a27671575cfd10e18ab2542c64509
krdesai999 <krdesai999@gmail.com>
2023-09-05 14:06:38 -0700

    user confirmation form added

--- src/pages/auth/ConfirmUser.js
+++ src/pages/auth/ConfirmUser.js
@@ -1,7 +1,42 @@
+import InputText from "../../components/utils/inputText";
+import { FormProvider, useForm } from "react-hook-form";
+import { verificationCodeConfig } from "../../components/config/formConfig";
+import { Navigate } from "react-router";
+import { useAuth } from "../../components/Auth";
+import { pages } from "../../components/utils/PageDirection";
+
 export default function ConfirmUser() {
-    return (
-        <div className="confirm-user" >
-            <h1>Confirm user</h1>
-        </div>
-    )
-};
+  const auth = useAuth();
+
+  const methods = useForm({
+    mode: "all",
+    defaultValues: {
+      verificationCode: "",
+    },
+  });
+
+  if (auth.user) {
+    if (auth.user.verified) return <Navigate to={pages.form} />;
+  } else {
+    return <Navigate to={pages.auth.login} />;
+  }
+
+  const onSubmit = methods.handleSubmit((data) => {
+    auth.verifyUserCode(data.verificationCode);
+  });
+
+  return (
+    <div className="user-confirmation-form">
+      <h1 className="text-center">User confirmation</h1>
+      <FormProvider {...methods}>
+        <form onSubmit={(e) => e.preventDefault()} noValidate>
+          <InputText {...verificationCodeConfig} />
+          <br />
+          <div className="submit btn text-center">
+            <button onClick={onSubmit}>Submit</button>
+          </div>
+        </form>
+      </FormProvider>
+    </div>
+  );
+}
