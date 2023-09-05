0868d9b799431c74bc0b93106fdc4cfa04ed0377
krdesai999 <krdesai999@gmail.com>
2023-09-05 13:59:53 -0700

    login form added

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,7 +1,46 @@
+import InputText from "../../components/utils/inputText";
+import { FormProvider, useForm } from "react-hook-form";
+import {
+  userNameConfig,
+  passwordConfig,
+} from "../../components/config/formConfig";
+import { Navigate } from "react-router";
+import { useAuth } from "../../components/Auth";
+import { pages } from "../../components/utils/PageDirection";
+
 export default function Login() {
-    return (
-        <div className="login" >
-            <h1>Login</h1>
-        </div>
-    )
-};
+  const auth = useAuth();
+
+  const methods = useForm({
+    mode: "all",
+    defaultValues: {
+      userName: "",
+      password: "",
+    },
+  });
+
+  if (auth.user) {
+    if (auth.user.verified) return <Navigate to={pages.form} />;
+    else return <Navigate to={pages.auth.confirm_user} />;
+  }
+
+  const onSubmit = methods.handleSubmit((data) => {
+    auth.login(data.email, data.password);
+  });
+
+  return (
+    <div className="login">
+      <h1>Login</h1>
+      <FormProvider {...methods}>
+        <form onSubmit={(e) => e.preventDefault()} noValidate>
+          <InputText {...userNameConfig} />
+          <br />
+          <InputText {...passwordConfig} />
+          <div>
+            <button onClick={onSubmit}>Submit</button>
+          </div>
+        </form>
+      </FormProvider>
+    </div>
+  );
+}
