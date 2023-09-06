6dc2a08fed70281136bfeb6eeaf0240d851da0d7
krdesai999 <krdesai999@gmail.com>
2023-09-06 00:15:23 -0700

    form not showing bug fix

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,7 +1,17 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../components/Auth";
+import { pages } from "../components/utils/PageDirection";
+
 export default function Form() {
-    return (
-        <div className="form" >
-            <h1>Input form</h1>
-        </div>
-    )
-};
+  const auth = useAuth();
+
+  if (auth.user) {
+    if (!auth.user.verified) return <Navigate to={pages.auth.confirm_user} />;
+  }
+
+  return (
+    <div className="form">
+      <h1>Input form</h1>
+    </div>
+  );
+}
