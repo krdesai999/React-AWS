7c226dcfed9743b99f317802ecfdb9dd5928b7cd
krdesai999 <krdesai999@gmail.com>
2023-09-06 18:59:57 -0700

    identity pool and s3 added

--- infra/lib/infra-stack.js
+++ infra/lib/infra-stack.js
@@ -1,9 +1,14 @@
-const { Stack, RemovalPolicy, SecretValue } = require('aws-cdk-lib');
+const {
+  IdentityPool,
+  UserPoolAuthenticationProvider,
+} = require("@aws-cdk/aws-cognito-identitypool-alpha");
+
+const { Stack, RemovalPolicy, SecretValue, CfnOutput } = require("aws-cdk-lib");
 const { UserPool } = require("aws-cdk-lib/aws-cognito");
 const { App, GitHubSourceCodeProvider } = require("@aws-cdk/aws-amplify-alpha");
+const { BlockPublicAccess, Bucket } = require("aws-cdk-lib/aws-s3");
 
 const config = require("../config.js");
-
 
 class InfraStack extends Stack {
   /**
@@ -15,9 +20,9 @@ class InfraStack extends Stack {
   constructor(scope, id, props) {
     super(scope, id, props);
 
-    // Cognito
+    // Cognito user pool
     const usersPool = new UserPool(this, "Users", {
-      userPoolName: "Users",
+      userPoolName: "UsersTest",
       signInCaseSensitive: false,
       selfSignUpEnabled: true,
       signInAliases: {
@@ -42,6 +47,32 @@ class InfraStack extends Stack {
     const client = usersPool.addClient("ReactApp");
     const clientID = client.userPoolClientId;
 
+    // identity pool
+    const identityPool = new IdentityPool(this, "myIdentityPool", {
+      identityPoolName: "myidentitypool",
+      authenticationProviders: {
+        userPool: [
+          new UserPoolAuthenticationProvider({
+            userPool: usersPool,
+            userPoolClient: client,
+          }),
+        ],
+      },
+    });
+
+
+    // s3
+    const myS3 = new Bucket(this, config.s3Config.bucketName, {
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+
+    myS3.addCorsRule(config.s3Config.corsRules);
+
+    // Grant permission to authenticated users
+    myS3.grantPut(identityPool.authenticatedRole);
+
     // Amplify
     const amplifyApp = new App(this, "Front-end-app", {
       description: "Frontend Code",
@@ -51,13 +82,22 @@ class InfraStack extends Stack {
         oauthToken: SecretValue.unsafePlainText(config.githubConfig.oauthToken),
       }),
       environmentVariables: {
-        CLIENT_ID: clientID,
+        S3_BUCKET: myS3.bucketName,
         USER_POOL_ID: userPoolID,
+        IDENTITY_POOL_ID: identityPool.identityPoolId,
+        CLIENT_ID: clientID,
       },
     });
 
     amplifyApp.addBranch(config.githubConfig.productionBranch);
+
+    // Print out in the console; url of the amplify lauched portal
+    new CfnOutput(this, "FrontEndUrl", {
+      value: amplifyApp.defaultDomain,
+      description: "URL for the deployed react app",
+      exportName: "FrontEndUrl",
+    });
   }
 }
 
-module.exports = { InfraStack }
+module.exports = { InfraStack };
