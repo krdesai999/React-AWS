10e8e26e4cfb0636f8b4468401b02f08b193f882
krdesai999 <krdesai999@gmail.com>
2023-09-07 07:49:53 -0700

    resource and instance profile integration

--- infra/resource/lambda/lmdec2trigger.py
+++ infra/resource/lambda/lmdec2trigger.py
@@ -10,38 +10,39 @@ INSTANCE_TYPE = 't2.micro'
 def handle_insert(record):
     print("Handling INSERT Event")
 
+    print("Getting new image")
     # Get newImage content
     newImage = record['dynamodb']['NewImage']
 
+    print("Parsing values")
     # Parse values
     id = newImage['id']['S']
-    inputText = newImage['inputText']['S']
+    inputText = newImage['input_text']['S']
     input_file_path = newImage['input_file_path']['S']
 
     try:
-        ec2 = boto3.client('ec2', region_name=os.environ.get("REGION"))
+        print("creating client")
+        ec2 = boto3.resource('ec2')
+        print("creating ec2")
         instance = ec2.create_instances(
             ImageId=AMI,
             InstanceType=INSTANCE_TYPE,
             MaxCount=1,
-            MinCount=1
+            MinCount=1,
+            IamInstanceProfile={
+                'Name': f'{os.environ.get("EC2_INSTANCE_PROFILE_NAME")}'
+            },
         )
 
-        waiter = ec2.get_waiter('instance_running')
         print("New instance created: ")
-        instance_id = instance['Instances'][0]['InstanceId']
-
-
-        response = ec2.associate_iam_instance_profile(
-            IamInstanceProfile={
-                'Name': os.environ.get("EC2_ROLE")
-            },
-                InstanceId=instance_id
-            )
-        print("Permission assigned!")
+        print(instance[0])
+        instance_id = instance[0].instance_id
         # time.sleep(111)
 
+        # SSM client creating
         ssm = boto3.client("ssm")
+
+        # Commands creating
         commands = [f"aws s3 cp s3://{os.environ.get('BUCKET_NAME')}/{os.environ.get('APPEND_TO_FILE_SCRIPT')} .",
             "python3 -m pip install boto3",
             f"export TABLE_NAME={os.environ.get('TABLE_NAME')}",
@@ -51,6 +52,7 @@ def handle_insert(record):
             f"aws s3 cp {input_file_path.split('/')[-1]} s3://{os.environ.get('BUCKET_NAME')}/output_{input_file_path.split('/')[-1]}"
             ]
 
+        # Sending command to ec2
         response = ssm.send_command(
             InstanceIds=[instance_id],
             DocumentName="AWS-RunShellScript",
@@ -73,6 +75,14 @@ def handle_insert(record):
         print(e)
     finally:
         time.sleep(50)
+        ec2.terminate()
+        ec2 = boto3.client('ec2')
+
+        response = ec2.terminate_instances(
+            InstanceIds=[
+                instance_id,
+            ],
+        )
         ec2.terminate_instances(InstanceIds=[instance_id])
         ec2.get_waiter('instance_terminated')
         print('Terminated instance: ' + str(instance_id))
@@ -84,12 +94,14 @@ def lambda_handler(event, context):
             if record['eventName'] == 'INSERT':
                 handle_insert(record)
 
+        print("Completed the ec2 trigger")
         return {
                 'statusCode': 200,
                 'body': json.dumps({'message': 'Success!'})
             }
 
     except:
+       print("Something went wrong in trigger")
        return {
                 'statusCode': 400,
                 'body': json.dumps({'message': 'Bad request'})
