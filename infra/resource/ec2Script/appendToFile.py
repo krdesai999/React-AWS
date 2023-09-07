cb4d3d899d1370ffacb22a03f32061522d819dd0
krdesai999 <krdesai999@gmail.com>
2023-09-07 14:22:54 -0700

    info added to ec2 append script

--- infra/resource/ec2Script/appendToFile.py
+++ infra/resource/ec2Script/appendToFile.py
@@ -3,15 +3,20 @@ import os
 
 import argparse
 
+print("Inside append to file in ec2")
 argParser = argparse.ArgumentParser()
 argParser.add_argument("-i", "--id", help="ID of the row")
 argParser.add_argument("-it", "--inputText", help="Input text to be append")
 argParser.add_argument("-ip", "--inputPath", help="Input Path of the uploaded file")
 
+print("Parsing the command line arguments")
 args = argParser.parse_args()
+
+print("creating client")
 s3 = boto3.resource('s3')
 dynamodb_client = boto3.client('dynamodb', region_name="us-east-1")
 
+print("getting item from table")
 #retrive data from DB
 response = dynamodb_client.get_item(
     TableName=os.environ.get("TABLE_NAME"),
@@ -21,11 +26,12 @@ response = dynamodb_client.get_item(
 )
 print(response['Item'])
 
+print("appending to file")
 # Append input text to file
 with open(args.inputPath.split('/')[-1], "a") as myfile:
     myfile.write("\n"+args.inputText)
 
-
+print("Updating the table")
 #add new entry in DB
 data = dynamodb_client.put_item(
     TableName=os.environ.get("TABLE_NAME"),
@@ -44,3 +50,5 @@ data = dynamodb_client.put_item(
         }
     }
 )
+
+print("Complete append file in ec2")
